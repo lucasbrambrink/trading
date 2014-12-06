d3c7dae293294b12d6ba2f23f752d12931439b2b
lucasbrambrink <lbrambrink@gmail.com>
2014-12-06 17:25:42 -0500

    SAM ALGORITHM WORKING

--- backtest/test_backtest.py
+++ backtest/test_backtest.py
@@ -1,22 +1,27 @@
+import os, django, sys
+sys.path.append('../')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graph_trader.settings.dev")
+django.setup()
+
 from backtest import *
 from models import Stocks,Prices
 import csv
 
 class CollectData:
 
-	def market_snapshot_by_date(self,date):
+	@staticmethod
+	def market_snapshot_by_date(date):
 		all_stocks = Prices.objects.filter(date=date)
 		day = { 'date' : date, 'data' : all_stocks}
 		return day
 
-	def market_snapshot_by_stock(self,symbol):
+	@staticmethod
+	def market_snapshot_by_stock(symbol):
 		stock = Stocks.objects.get(symbol=symbol)
 		all_prices = Prices.objects.filter(stock=stock)
 		stock = { 'symbol' : symbol, 'data' : all_prices}
 		return stock
 
-	def collect_stock_symbols(self):
-		return Stocks.objects.all()
 
 class ParseDates:
 
@@ -24,7 +29,7 @@ class ParseDates:
 		self.all_dates = self.prepare_dates_for_data_collection()
 
 	@staticmethod
-	def prepare_dates_for_data_collection(self):
+	def prepare_dates_for_data_collection():
 		all_dates = []
 		for year in range(2000,2014):
 			one_year = []
@@ -42,7 +47,7 @@ class ParseDates:
 		return all_dates
 
 	@staticmethod
-	def split_date_into_ints(self,date):
+	def split_date_into_ints(date):
 		year,month,day = date.split('-')
 		year = int(year)
 		if month[0] == '0':
@@ -55,16 +60,20 @@ class ParseDates:
 			day = int(day)
 		return year,month,day
 
-	@staticmethod
 	def collect_dates_in_range(self,start_date,end_date):
 		dates_in_range = []
 		start_year,start_month,start_day = self.split_date_into_ints(start_date)
 		end_year,end_month,end_day = self.split_date_into_ints(end_date)
 		for date in self.prepare_dates_for_data_collection():
 			year,month,day = self.split_date_into_ints(date)
-			if start_year <= year <= end_year:
-				if start_month <= month <= end_month:
+			if year == start_year:
+				if month >= start_month:
 					dates_in_range.append(date)
+			if start_year < year < end_year:
+				dates_in_range.append(date)
+			if year == end_year:
+				if month <= end_month:
+					dates_in_range.append(date) 
 		return dates_in_range
 
 
@@ -79,10 +88,9 @@ class SampleAlgorithm:
 		self.end_date = '2011-01-01' ## let's just try one year for now
 
 		## relevant dates ##
-		self.dates_in_range = self.collect_dates_in_range()
+		self.dates_in_range = ParseDates().collect_dates_in_range(self.start_date,self.end_date)
 		
-		# self.relevant_data = self.fetch_data_in_range()
-		self.stocks_in_market = self.cd.collect_stock_symbols()
+		self.stocks_in_market = Stocks.objects.all()
 
 		## calculator ##
 		self.c = Calculator()
@@ -93,44 +101,38 @@ class SampleAlgorithm:
 		## stocks to buy!!
 		self.stocks_to_buy = self.test_averages()
 
-	def fetch_data_in_range(self):
-		relevant_data = []
-		for date in self.dates_in_range:
-			days_stock_data = self.cd.market_snapshot_by_date(date)
-			relevant_data.append(days_stock_data)
-		return relevant_data
-
 	def get_simple_moving_averages(self):
 		averages = []
-		for symbol in self.stocks_in_market:
+		for stock_object in self.stocks_in_market:
+			stock = Stocks.objects.get(symbol=stock_object.symbol)
 			prices = []
-			this_symbol_data = self.cd.market_snapshot_by_stock(symbol)
-			for data in this_symbol_data['data']:
-				for date in self.dates_in_range:
-					if date == data['date']: ## date handshake
-						if len(prices) < 30: ## let's do a 30 day SMA
-							print(data['price'])
-							prices.append({'price' : data['price']})
-						else:
-							sma = self.c.average(prices,'price')
-							print(sma)
-							averages.append({
-								'symbol' : symbol,
-								'sma' : sma
-								})
-							prices = []
+			for date in self.dates_in_range[::-1]:
+				price = Prices.objects.filter(stock=stock).filter(date=date)
+				if len(price) > 0:
+					if len(prices) < 30: ## let's do a 30 day SMA
+						prices.append({'price' : float(price[0].close) })
+					else:
+						sma = self.c.average(prices,'price')
+						print(sma)
+						averages.append({
+							'symbol' : stock_object.symbol,
+							'sma' : sma
+							'date' : prices[-1].date
+							})
+						prices = []
 		return averages
 
 	def test_averages(self):
 		stocks_to_buy = []
-		for average in self.averages:
-			if average['sma'] > 0.1:
-				stocks_to_buy.append(average)
+		for index,average in enumerate(self.averages):
+			if index > 0:
+				pc_change = round(((average['sma'] - self.averages[index-1]['sma']) / (self.averages[index-1]['sma'])),5)
+				if pc_change > 0.10:
+					stocks_to_buy.append(average)
 		return stocks_to_buy
 
-
-
-print(SampleAlgorithm().test_averages())
+sa = SampleAlgorithm()
+print(sa.stocks_to_buy)
 
 
 
