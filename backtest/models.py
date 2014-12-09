49f7d3cd5cac9cb3dea84bf0faaf1a5ef581cb66
Derek Hu <derek.dchu@gmail.com>
2014-12-08 23:17:06 -0500

    Refactor models. Add index to stock ticker.

--- backtest/models.py
+++ backtest/models.py
@@ -1,60 +1,38 @@
 from django.db import models
 
 
-# Create your models here.
-
-class Users(models.Model):
-	email = models.EmailField(max_length=60)
-	created_at = models.DateTimeField(auto_now_add=True)
-	# friends = models.ForeignKey(Users)
-	avatar = models.CharField(max_length=10) ## if we have time
-
-	class Meta:
-		app_label = 'backtest'
-
-class Algorithms(models.Model):
-	created_by = models.ForeignKey(Users)
-	up_votes = models.IntegerField()
-
-	class Meta:
-		app_label = 'backtest'
-
-class Portfolios(models.Model):
-	## User Key
-	name = models.CharField(max_length=30)
-	algorithm = models.ForeignKey(Algorithms)
-	blocks = models.CharField(max_length=50) ## once we know what these are
-	balance = models.CharField(max_length=25)
-
-	class Meta:
-		app_label = 'backtest'
-
 class Stocks(models.Model):
-	name = models.CharField(max_length=100)
-	sector = models.CharField(max_length=100)
-	industry = models.CharField(max_length=100)
-	symbol = models.CharField(max_length=6)
+    name = models.CharField(max_length=100)
+    sector = models.CharField(max_length=100)
+    industry = models.CharField(max_length=100)
+    ticker = models.CharField(max_length=6, unique=True, db_index=True)
 
-	class Meta:
-		app_label = 'backtest'
-
-class Assets(models.Model):
-	portfolio = models.ForeignKey(Portfolios)
-	stock = models.ForeignKey(Stocks)
-	quantity = models.IntegerField()
-	price_purchased = models.CharField(max_length=10)
-
-	class Meta:
-		app_label = 'backtest'
 
 class Prices(models.Model):
-	stock = models.ForeignKey(Stocks)
-	date = models.CharField(max_length=15) ## dates are processed as strings
-	open = models.CharField(max_length=20)
-	high = models.CharField(max_length=20)
-	low = models.CharField(max_length=20)
-	close = models.CharField(max_length=20)
+    stock = models.ForeignKey(Stocks)
+    date = models.CharField(max_length=15) ## dates are processed as strings
+    open = models.CharField(max_length=20)
+    high = models.CharField(max_length=20)
+    low = models.CharField(max_length=20)
+    close = models.CharField(max_length=20)
 
-	class Meta:
-		app_label = 'backtest'
 
+"""
+class Algorithms(models.Model):
+    created_by = models.ForeignKey(Users)
+    up_votes = models.IntegerField()
+
+
+class Portfolios(models.Model):
+    name = models.CharField(max_length=30)
+    algorithm = models.ForeignKey(Algorithms)
+    blocks = models.CharField(max_length=50) ## once we know what these are
+    balance = models.CharField(max_length=25)
+
+
+class Assets(models.Model):
+    portfolio = models.ForeignKey(Portfolios)
+    stock = models.ForeignKey(Stocks)
+    quantity = models.IntegerField()
+    price_purchased = models.CharField(max_length=10)
+"""
