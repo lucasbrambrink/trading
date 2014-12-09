bf25fb4c55149b290180f5e9d4937671e8582830
Derek Hu <derek.dchu@gmail.com>
2014-12-09 03:02:33 -0500

    Restyle some codes.

--- graph_trader/settings/dev.py
+++ graph_trader/settings/dev.py
@@ -4,6 +4,10 @@ from .default import *
 
 DEBUG = True
 
-INSTALLED_APPS = DEFAULT_APPS + ('backtest','graph_trader','home','algo_builder')
+INSTALLED_APPS = DEFAULT_APPS + (
+    'backtest',
+    'home',
+    'algo_builder'
+)
 
 MIDDLEWARE_CLASSES = DEFAULT_MIDDLEWARE_CLASSES + ()
