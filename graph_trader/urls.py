cc071203d651730cbb0b213adb87634d159c3ccd
Derek Hu <derek.dchu@gmail.com>
2014-12-12 13:41:14 -0500

    Add back app_name in project urls.

--- graph_trader/urls.py
+++ graph_trader/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-	url(r'^$', include('home.urls', namespace='homepage')),
-	url(r'^builder/$', include('algo_builder.urls', namespace='builder')),
-    url(r'^backtest/$', include('backtest.urls', namespace='backtest')),
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('home.urls', namespace='homepage', app_name='home')),
+    url(r'^builder/', include('algo_builder.urls', namespace='builder-app', app_name='algo_bulider')),
+    url(r'^backtest/', include('backtest.urls', namespace='backtest-app', app_name='backtest')),
+    url(r'^admin/', include(admin.site.urls)),
 )
