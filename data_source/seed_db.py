87d3c729fb320c3831377a57bbc3e912a90a75f2
Derek Hu <derek.dchu@gmail.com>
2014-12-12 13:41:47 -0500

    Update seed_db script.

--- data_source/seed_db.py
+++ data_source/seed_db.py
@@ -7,17 +7,16 @@ import os, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graph_trader.settings.dev")
 import django
 django.setup()
-from django.db.models import ObjectDoesNotExist
 
 from backtest.models import Stocks, Prices
 
 import csv
 
 # CSV file storing all stock prices
-prices_csv = './csv/stock_prices.csv'
+prices_csv = './csv/prices_no_errors.csv'
 
 # Valid ticker list
-ticker_csv = './csv/valid-ticker-list.csv'
+ticker_csv = './csv/ticker-list.csv'
 
 def seed_stock_info():
     """
@@ -32,8 +31,11 @@ def seed_stock_info():
 
             # Read data
             for row in data_reader:
-                Stocks.objects.create(name=row['Name'], sector=row['Sector'], industry=row['Industry'],
-                                      ticker=row['Ticker'])
+                try:
+                    Stocks.objects.get(symbol=row['Name'])
+                except Stocks.DoesNotExist:
+                    Stocks.objects.create(name=row['Name'], sector=row['Sector'], industry=row['Industry'],
+                                      symbol=row['Ticker'])
 
             print('Finish seeding stock information.')
             return True
@@ -41,7 +43,7 @@ def seed_stock_info():
         print("Unexpected error:", sys.exc_info()[0])
         return False
 
-def seed_stock_price():
+def seed_stock_price(limit):
     """
     Seed prices table with Date, Open, High, Low, Close
 
@@ -50,38 +52,48 @@ def seed_stock_price():
 
     # Check if it contains data
     try:
-        price = Prices.objects.get(pk=1000000)
+        Prices.objects.get(pk=1000000)
         ans = input('It seems there are 1000000 data inside, do you still want to seed the table? Y/N\n')
         if ans != 'Y':
             print('Abort seeding!')
-    except:
+    except Prices.DoesNotExist:
         # There is no data
         pass
 
+    count = 0
+    stock = Stocks.objects.get(pk=1)
     try:
         with open(prices_csv, newline='') as prices_f:
             data_reader = csv.reader(prices_f, delimiter=',')
+            # Skip fieldnames
+            next(data_reader)
 
             # Read data
-            for row in data_reader:
-                if len(row) != 6:
-                    # Info row
+            while count < limit:
+                row = next(data_reader)
+                if row[0] != stock.symbol:
+                    # New stock
                     try:
-                        stock = Stocks.objects.get(ticker=row[0])
-                    except ObjectDoesNotExist:
+                        stock = Stocks.objects.get(symbol=row[0])
+                    except Stocks.DoesNotExist:
                         stock = None
 
                 elif stock is not None:
                     # price row and stock is valid
-                    Prices.objects.create(stock=stock, date=row[1], open=row[2], high=row[3], low=row[4], close=row[5])
+                    try:
+                        Prices.objects.create(stock=stock, date=row[1], open=row[2], high=row[3],
+                                              low=row[4], close=row[5], volume=row[6])
+                        count += 1
+                    except Exception:
+                        print("Unexpected error:", sys.exc_info())
 
             print('Finish seeding stock prices.')
             return True
     except Exception:
-        print("Unexpected error:", sys.exc_info()[0])
+        print("Unexpected error:", sys.exc_info())
         return False
 
 
 if __name__ == '__main__':
     print(seed_stock_info())
-    print(seed_stock_price())
+    print(seed_stock_price(10000))
