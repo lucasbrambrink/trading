f0ffc61a3656c6008caab167d4bedf575e57e05d
Derek Hu <derek.dchu@gmail.com>
2014-12-14 01:02:45 -0500

    Create urls for post run request and show result.

--- backtest/urls.py
+++ backtest/urls.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 from backtest.views import *
 
 urlpatterns = patterns('',
-	url(r'^$', RootView.as_view(), name='root'),
+    url(r'^$', RootView.as_view(), name='root'),
+    url(r'^run/', run, name='run'),
+    url(r'^result/(?P<id>[0-9a-zA-Z]+)/$', ResultView.as_view(), name='result'),
+    url(r'^result/(?P<id>[0-9a-zA-Z]+)/(?P<num>[0-9]+)/', data, name='data')
 )
