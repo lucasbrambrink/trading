d0647743781e34ea5c8079e0c23cbefe8055a922
Derek Hu <derek.dchu@gmail.com>
2014-12-13 19:57:52 -0500

    Update algo_builder ulrs and views. Now, when user click on new algorithm, it will redirect to the builder page with an unique id.

--- algo_builder/views.py
+++ algo_builder/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render,redirect
 from django.http import JsonResponse
-from django.views.generic.base import View
+from django.core.urlresolvers import reverse
+from django.views.generic.base import TemplateView
 
-# Create your views here.
 
-class RootView(View):
-	template_name = 'algo_builder/builder.html'
-	
-	def get(self,request):
-		return render(request, self.template_name)
+from uuid import uuid1
 
-	def post(self,request):
-		pass
+def UUIDView(request):
+    id = uuid1().hex
+    return redirect(reverse('builder:builder', kwargs={'id': id}))
+
+
+class BuilderView(TemplateView):
+    template_name = 'algo_builder/builder.html'
+
+    def get_context_data(self, **kwargs):
+        content = super(BuilderView, self).get_context_data(**kwargs)
+        content['id'] = self.kwargs.get('id', None)
+        return content
