a0d6ba44878e4bf25d66ca5bcf266646106be5d0
Derek Hu <derek.dchu@gmail.com>
2014-12-13 19:53:26 -0500

    Update namespace for urls.

--- graph_trader/urls.py
+++ graph_trader/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', include('home.urls', namespace='homepage', app_name='home')),
-    url(r'^builder/', include('algo_builder.urls', namespace='builder-app', app_name='algo_bulider')),
-    url(r'^backtest/', include('backtest.urls', namespace='backtest-app', app_name='backtest')),
+    url(r'^builder/', include('algo_builder.urls', namespace='builder', app_name='algo_bulider')),
+    url(r'^backtest/', include('backtest.urls', namespace='backtest-runner', app_name='backtest')),
     url(r'^admin/', include(admin.site.urls)),
 )
