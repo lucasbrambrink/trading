e9c39005399d4461fad3ad6a8cdad2ba045c083f
Derek Hu <derek.dchu@gmail.com>
2014-12-15 16:19:37 -0500

    Add back settings for Celery within dev settings.

--- graph_trader/settings/dev.py
+++ graph_trader/settings/dev.py
@@ -5,11 +5,21 @@ from .default import *
 DEBUG = True
 
 INSTALLED_APPS = DEFAULT_APPS + (
-    # 'project_core',
-    'backtest',
+    'rest_framework',
+
+    #'project_core',
     'home',
     'algo_builder',
-    'rest_framework',
+    'backtest',
 )
 
 MIDDLEWARE_CLASSES = DEFAULT_MIDDLEWARE_CLASSES + ()
+
+# Rabbitmq Settings
+RABBITMQ_HOST = 'localhost'
+
+# Celery Settings
+BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
