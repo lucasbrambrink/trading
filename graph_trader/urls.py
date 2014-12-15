18d5963dc529441a60f7cd3b14a44ba45564f9d5
Derek Hu <derek.dchu@gmail.com>
2014-12-15 17:27:35 -0500

    Add default django auth app urls.

--- graph_trader/urls.py
+++ graph_trader/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', include('home.urls', namespace='homepage', app_name='home')),
+    url(r'^auth', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^social/', include('social.apps.django_app.urls', namespace='social', app_name='python-social-auth')),
     url(r'^builder/', include('algo_builder.urls', namespace='builder', app_name='algo_bulider')),
     url(r'^backtest/', include('backtest.urls', namespace='backtest-runner', app_name='backtest')),
