98cd4e64356f647d2f256deb742bed62f25f16b9
Derek Hu <derek.dchu@gmail.com>
2014-12-16 14:45:12 -0500

    Fix an import error in backtest models.

--- backtest/models.py
+++ backtest/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from managers import StockManager
+from backtest.managers import StockManager
 
 
 class Stocks(models.Model):
