62132612b4dd5d35ab206aefec243dde81ae0b18
Derek Hu <derek.dchu@gmail.com>
2014-12-15 23:02:15 -0500

    Add django-user-accounts urls.

--- graph_trader/urls.py
+++ graph_trader/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', include('home.urls', namespace='homepage', app_name='home')),
     url(r'^auth', include('django.contrib.auth.urls', namespace='auth')),
+    url(r'^account/', include('account.urls')),
     url(r'^social/', include('social.apps.django_app.urls', namespace='social', app_name='python-social-auth')),
     url(r'^builder/', include('algo_builder.urls', namespace='builder', app_name='algo_bulider')),
     url(r'^backtest/', include('backtest.urls', namespace='backtest-runner', app_name='backtest')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('home.urls', namespace='homepage', app_name='home')),
 )
