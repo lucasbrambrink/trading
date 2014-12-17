0af91a088c02c7a85a4d36cb967ed4f43c6476a9
Derek Hu <derek.dchu@gmail.com>
2014-12-17 01:57:33 -0500

    Create NewView in algo_builder for building new algorithm.

--- algo_builder/views.py
+++ algo_builder/views.py
@@ -6,17 +6,17 @@ from django.views.generic.base import TemplateView
 
 from uuid import uuid1
 
-def UUIDView(request):
-    id = uuid1().hex
-    return redirect(reverse('builder:builder', kwargs={'id': id}))
+def uuid_view(request):
+    algo_id = uuid1().hex
+    return redirect(reverse('builder:new', kwargs={'algo_id': algo_id}))
 
 
-class BuilderView(TemplateView):
+class NewView(TemplateView):
     template_name = 'algo_builder/builder.html'
 
     def get_context_data(self, **kwargs):
-        content = super(BuilderView, self).get_context_data(**kwargs)
-        content['id'] = self.kwargs.get('id', None)
+        content = super(NewView, self).get_context_data(**kwargs)
+        content['algo_id'] = self.kwargs.get('algo_id', None)
         return content
 
 
