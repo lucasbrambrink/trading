ae6721f8210544302788270610582bbd200bcee7
lucasbrambrink <lbrambrink@gmail.com>
2014-12-17 17:28:43 -0500

    jquery pretty good with blocks, still needs to return block to original place after restart

--- algo_builder/static/algo_builder/builder.js
+++ algo_builder/static/algo_builder/builder.js
@@ -1,13 +1,29 @@
 description_text = {
 	'SMA': 'The simple moving average calculates the mean stock price for two consecutive periods and compares their percent difference.',
-	'Volatility': "The volatility of a given stock is defined by it's standard deviation, the average difference from the mean per data point.",
+	'Volatility': "The volatility of a given stock is defined by its standard deviation, the average difference from the mean. This block allows you to target more volatile stocks or stay away from them depending on your risk appetite. It is scored as a percentage of the stock price (e.g. 0.1 means the stock deviates from its mean price by an average of 10% of its value.",
 	'Covariance': 'Hello',
 	'Event': "This is soemthing",
 	'Thresholds': "A passive condition, this places limits on how your portfolio may behave.",
 	'Diversity': "By diversifying your portfolio, you broaden your base."
 }
 
+function resetForm(A,B){
+	document.getElementById(A).reset();
+	document.getElementById(B).reset();
+}
+
 $(document).ready(function(){	
+	final_json_object = $.cookie('final_json_object')
+	if(!final_json_object){
+		final_json_object = {}
+		$.cookie('final_json_object', final_json_object)
+	}
+	algorithm_text = $.cookie('algorithm_text')
+	if(!algorithm_text){
+		algorithm_text = []
+		$.cookie('algorithm_text', algorithm_text)
+	}
+
 	$('.popup_conditions').hide();
 	$('.draggable').draggable();
 	$('#SMA').data({
@@ -32,12 +48,14 @@ $(document).ready(function(){
 			.css({
 				'top': $("#SMA").data('origionalTop'),
 				'left': $("#SMA").data('origionalLeft'),
-			}) // ('display','block');
+			})
     });
 
 
 	$('.conditionals').submit(function(e){
 		e.preventDefault();
+		form_id = '#' + $(this).attr('id')
+		virgin_form_id = $(this).attr('id')
 		post_data = $(this).serialize() + '&id=' + $(this).attr('id').split('_')[0]	
 		$('#behavior_conditions').show()
 				.animate({
@@ -50,6 +68,19 @@ $(document).ready(function(){
 
 	$('#behavior_form').submit(function(e){
 		e.preventDefault();
+		resetForm(virgin_form_id,'behavior_form')
+		$('.popup_conditions').animate({
+			width: '0px',
+			height: '0px',
+			opacity: 0,
+			borderWidth: '0px'
+		}, 300);
+		$('#behavior_form').animate({
+			width: '0px',
+			height: '0px',
+			opacity: 0,
+			borderWidth: '0px'
+		}, 300);
 		var behavior = $(this).serialize().split("=")[0]
 		post_data += '&behavior=' + behavior
 		console.log(post_data)
@@ -61,24 +92,62 @@ $(document).ready(function(){
 	                data: post_data
 	            },
 	            success: function (data) {
+	            	var key = Object.keys(data.block)[0]
+	 				if(data.block[key]['buy'].length > 0){
+	 					var behavior = 'buy'
+	 				} else {
+	 					var behavior = 'sell'
+	 				}
+	 				console.log(data.block[key][behavior][0])
+	            	if(key === 'sma'){
+	            		var condition = 'IF the average price of the last ' + data.block[key][behavior][0]['period1'] + ' days differs from the previous ' + data.block[key][behavior][0]['period2'] +' days by a percent difference between ' + data.block[key][behavior][0]['range'][0] + '% and ' + data.block[key][behavior][0]['range'][1] +'%, THEN ' + behavior + ' with an appetite of ' + data.block[key][behavior][0]['appetite']
+	               		var find_block_id = '#SMA'
+	               	} else if(key === 'volatility' || key === 'covariance') {
+	               		console.log(key)
+	               		if(key==='volatility'){var find_block_id = '#Volatility'}
+               			else {var find_block_id = '#Covariance'}
+	            	} else if(key === 'event') {
+	            		console.log(key)
+	            		var find_block_id = '#Event'
+	            	} else if(key === 'ratio'){
+	            		console.log(key)
+	            		var find_block_id = '#Ratio'
+	            	} else if(key === 'thresholds') {
+	            		console.log(key)
+	            		var find_block_id = '#Thresholds'
+	            	} else if(key === 'diversity') {
+	            		console.log(key)
+	            		var find_block_id = '#Diversity'
+	            	}
+	            	$('#conditions_list').append("<li><h1>"+condition+"</h1></li>")
+	                
+	            	// Store in Browser
+	                final_json_object = $.cookie('final_json_object')
+	                final_json_object[key] = data.block
+	                $.cookie('final_json_object',final_json_object)
+	                console.log(final_json_object)
 	                console.log(data);
-	                console.log("submit form");
+
+    				$(find_block_id).show()
+						.css({
+							'top': $("#SMA").data('origionalTop'),
+							'left': $("#SMA").data('origionalLeft'),
+						})
 	            },
 	            error: function (xhr, errmsg, err) {
 	                alert("error");
 	            }
-	        });
+	        },'json');
 	});
 
 	$("#behavior_cancel").click(function() {
-		$('.popup_conditions').hide();
-		// $('.conditionals').hide()
-		// 	.animate({
-		// 	width: '0px',
-		// 	height: '0px',
-		// 	opacity: 0,
-		// 	borderWidth: '0px'
-		// }, 300);
+		$('.popup_conditions').animate({
+			width: '0px',
+			height: '0px',
+			opacity: 0,
+			borderWidth: '0px'
+		}, 300);
+
 		var splits = post_data.split('&')
 		var block_id_pair = splits[splits.length-1]
 		var block_id = "#" + block_id_pair.split('=')[1]
@@ -93,7 +162,6 @@ $(document).ready(function(){
  		drop: function(event, ui) {
  			var match_to_block = $(ui.draggable).attr('id')
 			var match_to_form = "#" + $(ui.draggable).attr('id') + "_conditions";
-			console.log(match_to_form)
  			$(match_to_form).show()
  				.animate({
 				    width: "22%",
@@ -101,26 +169,19 @@ $(document).ready(function(){
 				    opacity: 1,
 				    borderWidth: "4px"
 				  }, 300 );
- 			// possible to run AJAX post/get request if we want
- 			var position_left = match_to_block + "_original_left"
- 				position_top = match_to_block + "_original_top"
  			$(ui.draggable).hide()
- 			// $(ui.draggable).css({
-			 //        'left': $(ui.draggable).data(position_left),
-			 //        'top': $(ui.draggable).data(position_top)
-			 //    });
-    		}
-  		});
+		}
+	});
 
-	    $('.draggable').hover(
-	    	function() {
-            	var id = $(this).attr('id');
-            	$('.description_header')
-            		.text(id)
-            	$('.description_text')
-            		.text(description_text[id])
+    $('.draggable').hover(
+    	function() {
+        	var id = $(this).attr('id');
+        	$('.description_header')
+        		.text(id)
+        	$('.description_text')
+        		.text(description_text[id])
 
-            })
+    });
 
 
 });
--- algo_builder/views.py
+++ algo_builder/views.py
@@ -34,16 +34,12 @@ class JsonBuilder(TemplateView):
 			try:
 				buy,sell = [],[]
 				data = request.POST['data'].split('&')
+				print(data)
 				block = {}
 				for item in data:
 					key,value = item.split('=')
 					block[key] = value
-				formatted_block = {
-						'period1' : block['period1'],
-						'period2' : block['period2'],
-						'range': (block['range0'],block['range1']),
-						'appetite': block['appetite']
-						}
+				formatted_block = self.parse_block(block)
 				if block['behavior'] == 'buy':
 					buy.append(formatted_block)
 				if block['behavior'] == 'sell':
@@ -59,3 +55,43 @@ class JsonBuilder(TemplateView):
 
 		else:
 			return Http404
+
+
+	def parse_block(self,block):
+		if block['id'].lower() == 'sma':
+			formatted_block = {
+					'period1' : block['period1'],
+					'period2' : block['period2'],
+					'range': (block['range0'],block['range1']),
+					'appetite': block['appetite']
+					}
+		if block['id'].lower() == 'volatility' or block['id'].lower() == 'covariance':
+			formatted_block = {
+					'period1' : block['period'],
+					'range': (block['range0'],block['range1']),
+					'appetite': block['appetite']
+					}
+		if block['id'].lower() == 'event':
+			formatted_block = {
+					'stock' : block['stock'],
+					'attribute': 'close',
+					'range': (block['range0'],block['range1']),
+					'appetite': block['appetite']
+					}
+		if block['id'].lower() == 'ratio':
+			formatted_block = {
+					'name' : block['name'],
+					'range': (block['range0'],block['range1']),
+					'appetite': block['appetite']
+					}
+		if block['id'].lower() == 'thresholds':
+			formatted_block = {
+					'price_range' : (block['price_range0'],block['price_range1']),
+					'sector': {block['inout']: block['sector']}
+					}
+		if block['id'].lower() == 'diversity':
+			formatted_block = {
+					'num_sector' : block['num_sector'],
+					'num_industry': block['num_industry']
+					}
+		return formatted_block
