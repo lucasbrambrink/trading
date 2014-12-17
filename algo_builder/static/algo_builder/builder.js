52c511741deeeb743882d3fa93351bff02cf3236
lucasbrambrink <lbrambrink@gmail.com>
2014-12-16 19:49:40 -0500

    hover for text, incidentally aligns the divs i dont know why not before

--- algo_builder/static/algo_builder/builder.js
+++ algo_builder/static/algo_builder/builder.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){	
+	$('#sma_conditions').hide();
 	$('.draggable').draggable();
 
 	$('.droppable').droppable({	
  		drop: function(event, ui) {
+ 			$('#sma_conditions').show()
+ 				.animate({
+				    width: "45%",
+				    height: "55%",
+				    opacity: 1,
+				    fontSize: "3em",
+				    borderWidth: "4px"
+				  }, 500 );
  			// possible to run AJAX post/get request if we want
+ 			$(ui.draggable).hide()
  			$(ui.draggable).css('color','white');
     		$( this )
 	          .css('background-color','green')
@@ -13,4 +23,17 @@ $(document).ready(function(){
             $(this.ui)
     		}
   		});
+
+	    $('.draggable').hover(
+	    	function() {
+            	var id = $(this).attr('id');
+            	console.log(id)
+            	console.log()
+            	$('.description_header')
+            		.text(id)
+            	$('.description_text')
+            		.text("SMA is very interesting indeed")
+
+            })
+
 });
