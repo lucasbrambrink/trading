a2d013d86e6e75984c2df4d9db3381feb79a8c7a
Derek Hu <derek.dchu@gmail.com>
2014-12-18 00:49:12 -0500

    Update test_backtest task

--- backtest/tasks.py
+++ backtest/tasks.py
@@ -1,12 +1,14 @@
 from __future__ import absolute_import
 
 from time import mktime
+import json
 
 from celery import shared_task
 
-from .algorithm import BaseAlgorithm
-from .backtest import BacktestingEnvironment
-from .queues import ReturnsQueue
+from backtest.algorithm import BaseAlgorithm
+from backtest.backtest import BacktestingEnvironment
+from backtest.queues import ReturnsQueue
+from backtest.models import Algorithms
 
 
 @shared_task
@@ -41,14 +43,21 @@ def run_backtest(algorithm_json):
     backtest.run()
 
 @shared_task
-def test_backtest(algorithm_json):
+def test_backtest(algo_id, backtest_environ):
     import math
     from random import random
     from .queues import ReturnsQueue
 
+    print('Celery: start backtest!!!')
+
+    algorithm = Algorithms.objects.get(uuid=algo_id)
+    algorithm_json = {
+        'algorithm': json.loads(algorithm.json_string)
+    }
     base = BaseAlgorithm(algorithm_json['algorithm'])
-    backtest = BacktestingEnvironment(algorithm_json['backtest'], base.__dict__)
-    returns_queue = ReturnsQueue(backtest.id)
+    backtest = BacktestingEnvironment(backtest_environ, base.__dict__)
+    print(backtest.start_date, backtest.end_date, backtest.dates_in_range)
+    returns_queue = ReturnsQueue(backtest.uuid)
 
     for index, date in enumerate(backtest.dates_in_range):
         if index % math.floor(252/backtest.frequency) == 0:
