7bc88e15985a1f692d53ae14c04ed7eb642c812c
Derek Hu <derek.dchu@gmail.com>
2014-12-18 01:19:40 -0500

    Add realtime view url.

--- backtest/urls.py
+++ backtest/urls.py
@@ -4,7 +4,7 @@ from backtest.views import *
 
 urlpatterns = patterns('',
     url(r'^run/(?P<algo_id>[0-9a-zA-Z]{32})$', RunView.as_view(), name='run'),
-    #url(r'^realtime/(?P<backtest_id>[0-9a-zA-Z]{32})/(?P<num>[0-9]+)/', realtime, name='realtime'),
+    url(r'^realtime/(?P<backtest_id>[0-9a-zA-Z]{32})/(?P<num>[0-9]+)/', realtime_view, name='realtime'),
     # url(r'^history/$', list all algorithms)
     # url(r'^history/(?P<algo_id>[0-9a-zA-Z]{32})/$', list all backtests)
     # url(r'^history/(?P<algo_id>[0-9a-zA-Z]{32})/(?P<backtest_id>[0-9a-zA-Z]{32})$', show backtest result)
