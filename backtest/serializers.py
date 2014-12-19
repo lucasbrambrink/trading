91af47f1e199c5c4f4af2bc1ec2cfff379f9386e
Derek Hu <derek.dchu@gmail.com>
2014-12-18 23:47:32 -0500

    Create serializer for RiskMetrics.

--- backtest/serializers.py
+++ backtest/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from backtest.models import Stocks, Assets
+from backtest.models import (Stocks, Assets, RiskMetrics)
 
 
 class StocksSerializer(serializers.ModelSerializer):
@@ -7,9 +7,16 @@ class StocksSerializer(serializers.ModelSerializer):
         model = Stocks
         fields = ('name', 'sector', 'industry', 'symbol')
 
+
 class AssetsSerializer(serializers.ModelSerializer):
     stock = StocksSerializer()
 
     class Meta:
         model = Assets
         fields = ('stock', 'quantity', 'price_purchased', 'date')
+
+
+class RiskMetricsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RiskMetrics
+        fields = ('date', 'alpha', 'beta' ,'sharpe', 'volatility', 'returns')
