26b087e073fbbdcf8d295b8bd8eac9b203225203
Derek Hu <derek.dchu@gmail.com>
2014-12-18 23:48:06 -0500

    Create API View for serving RiskMetrics of a backtest with a date.

--- backtest/views.py
+++ backtest/views.py
@@ -12,8 +12,8 @@ from account.mixins import LoginRequiredMixin
 
 from backtest.tasks import run_backtest
 from backtest.queues import ReturnsQueue
-from backtest.models import Backtests, Assets
-from backtest.serializers import AssetsSerializer
+from backtest.models import (Backtests, Assets, RiskMetrics)
+from backtest.serializers import (AssetsSerializer, RiskMetricsSerializer)
 
 
 class JSONResponse(JsonResponse):
@@ -96,4 +96,39 @@ class AssetsDateList(APIView):
         date = '{}-{}-{}'.format(year, month, day)
         assets = Assets.objects.filter(backtest=backtest, date=date)
         serializer = AssetsSerializer(assets, many=True)
+        return Response(serializer.data)
+
+
+class RiskMetricsList(APIView):
+    """
+    List all risk metrics for a backtest
+    """
+    def get_object(self, uuid):
+        try:
+            return Backtests.objects.get(uuid=uuid)
+        except Backtests.DoesNotExist:
+            raise Http404
+
+    def get(self, request, backtest_uuid, format=None):
+        backtest = self.get_object(backtest_uuid)
+        risks = RiskMetrics.objects.filter(backtest=backtest)
+        serializer = RiskMetricsSerializer(risks, many=True)
+        return Response(serializer.data)
+
+
+class RiskMetricsDateList(APIView):
+    """
+    List all assets for a backtest
+    """
+    def get_object(self, uuid):
+        try:
+            return Backtests.objects.get(uuid=uuid)
+        except Backtests.DoesNotExist:
+            raise Http404
+
+    def get(self, request, backtest_uuid, year, month, day, format=None):
+        backtest = self.get_object(backtest_uuid)
+        date = '{}-{}-{}'.format(year, month, day)
+        risks = RiskMetrics.objects.filter(backtest=backtest, date=date)
+        serializer = RiskMetricsSerializer(risks, many=True)
         return Response(serializer.data)
